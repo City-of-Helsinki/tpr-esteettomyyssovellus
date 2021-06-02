8feec23f2a490db8da191d5a893f92a27087b7d4
Kaunisto <leevi.kaunisto@cgi.com>
2021-06-02 11:40:25 +0300

    Add basic custom Form model.

--- accessibility/esteettomyyssovellus/api/serializers.py
+++ accessibility/esteettomyyssovellus/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-
+from . models import Form
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +12,9 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class FormSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Form
+        fields = ['form_id']
--- accessibility/esteettomyyssovellus/api/views.py
+++ accessibility/esteettomyyssovellus/api/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from . serializers import UserSerializer, GroupSerializer
-
+from . serializers import FormSerializer, UserSerializer, GroupSerializer
+from . models import Form
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -20,3 +20,12 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class FormViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Form.objects.all()
+    serializer_class = FormSerializer
+    # permission_classes = [permissions.IsAuthenticated]
