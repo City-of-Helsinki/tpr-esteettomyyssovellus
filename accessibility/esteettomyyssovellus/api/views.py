7f8a23e020f7718baf36287c4434fb5d3bf9ee8e
Kaunisto <leevi.kaunisto@cgi.com>
2022-05-30 11:50:37 +0300

    Add filtering by entrance_id to ArXQuestionBlockAnswerCmt.

--- accessibility/esteettomyyssovellus/api/views.py
+++ accessibility/esteettomyyssovellus/api/views.py
@@ -1975,7 +1975,7 @@ class ArXPlaceAnswerBoxViewSet(viewsets.ModelViewSet):
             box_id = getattr(answer_box, "box_id")
             if box_id == None:
                 return Response(
-                    "adadad",
+                    "Deletion failed.",
                     status=status.HTTP_400_BAD_REQUEST,
                 )
 
@@ -2022,6 +2022,16 @@ class ArXQuestionBlockAnswerCmtViewSet(viewsets.ModelViewSet):
     permission_classes = [
         TokenPermission,
     ]
+
+    def get_queryset(self):
+        if self.request.method == "GET":
+            queryset = ArXQuestionBlockAnswerCmt.objects.all()
+            entrance_id = self.request.GET.get("entrance_id", None)
+            if entrance_id is not None:
+                logs = ArXAnswerLog.objects.all().filter(entrance_id=entrance_id)
+                log_ids = [log.log_id for log in logs]
+                queryset = queryset.filter(log_id__in=log_ids)
+            return queryset
 
 
 class ArXQuestionBlockAnswerViewSet(viewsets.ModelViewSet):
